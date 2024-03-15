72915658bd6b66cd486fb683adeb8db6602a8886
sephiroth613 <inglopezcala@gmail.com>
2024-03-15 14:43:29 -0500

    Actualizar configuración de conexión a la base de datos para omitir la verificación del certificado SSL/TLS

--- config/database.php
+++ config/database.php
@@ -60,6 +60,7 @@ return [
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+                PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
             ]) : [],
         ],
 
@@ -101,6 +102,10 @@ return [
             'username' => env('DB_USERNAME_CONTABILIDAD', 'antares'),
             'password' => env('DB_PASSWORD_CONTABILIDAD', 'dic2401'),
             // ...
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+                PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
+            ]) : [],
         ],
 
     ],
